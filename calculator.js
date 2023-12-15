24dace88faa073466f5b957b9f91ec25442703f0
Alexey <Alexey@DESKTOP-PE694DS>
2023-12-15 12:00:15 +0300

    feat: add digits logic and buttons for opareations

--- calculator.js
+++ calculator.js
@@ -1,1 +1,18 @@
-document.getElementById("buttonNine").onclick = function () { console.log('Nine'); };
+const digits = document.getElementsByClassName("digit")
+const dot = document.getElementById("buttonDot")
+const result = document.getElementById("result")
+result.value = '0'
+
+for(var i = 0; i < digits.length; i++)
+{
+    digits[i].addEventListener('click', function(){
+        if(result.value == 0)
+            result.value = ''
+        result.value += this.value;
+    })
+}
+
+dot.addEventListener('click', function(){
+    if(!(result.value.includes('.')))
+        result.value += this.value
+})
